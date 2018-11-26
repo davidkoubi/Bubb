c3939fcf0508aadff778a711a7638f1b7288988a
Developer <developer@Developers-Mac.local>
2018-11-26 13:58:07 +0100

    Geoloc

--- Bubble/View Controllers/ViewController.swift
+++ Bubble/View Controllers/ViewController.swift
@@ -10,15 +10,26 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , CLLocationManagerDelegate, MKMapViewDelegate {
 
-    @IBOutlet weak var TextButton: UIButton!
-    @IBOutlet weak var ConfirmedPasswordTextField: UITextField!
-    @IBOutlet weak var PasswordTextField: UITextField!
-    @IBOutlet weak var PseudoTextField: UITextField!
+    
+    @IBOutlet weak var MapViewMKMapView: MKMapView!
+    let manager = CLLocationManager()
+    
+   
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+      
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.distanceFilter = 100
+       //manager.requestWhenInUseAuthorization()
+       manager.requestAlwaysAuthorization()
+        manager.startUpdatingLocation()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        MapViewMKMapView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,11 +37,73 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func ConnectButton(_ sender: Any) {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        super.viewDidLoad()
+        //Select current location
+        let location = locations[0]
+        //Select precision
+        let span : MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        //Create current location
+        let myLocation :CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        //Create region
+        let region :MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        
+        //print(location.coordinate.latitude, location.coordinate.longitude)
+        
+        //Set up region our map with map sliding
+        MapViewMKMapView.setRegion(region, animated: true)
+        //Show User on map
+        self.MapViewMKMapView.showsUserLocation = true
+        
+        //Init les point autour de lui
+        addBubble()
         
     }
     
+    let address:[[String]] = [["Boris","37.33024596","-122.0279578","200.0"],["Naked","37.33024947","-122.0273368","20.0"],["Test","37.32475245" ,"-122.02135932","180.0"]]
     
+    func addBubble(){
+        
+        for code in address {
+            let title = code[0]
+            let coordinate = CLLocationCoordinate2DMake(Double(code[1])!,Double(code[2])!)
+            let regionRadius = Double(code[3])!
+            
+            
+            let regioncircle = CLCircularRegion(center: CLLocationCoordinate2D(latitude: coordinate.latitude,longitude: coordinate.longitude), radius: regionRadius, identifier: title)
+            
+            manager.startMonitoring(for: regioncircle)
+            
+            let Bubble = MKPointAnnotation()
+            Bubble.coordinate = coordinate;
+            Bubble.title = "\(title)";
+            MapViewMKMapView.addAnnotation(Bubble)
+            
+           // let circle = MKCircle(center: coordinate, radius: regionRadius)
+           // MapViewMKMapView.add(circle)
+        
+        }
+        
+    }
     
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let circleRenderer = MKCircleRenderer(overlay: overlay)
+        circleRenderer.strokeColor = UIColor.red
+        circleRenderer.lineWidth = 1.0
+        return circleRenderer
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        
+        print("exit \(region.identifier)")
+        
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+      
+        print("enter \(region.identifier)")
+    }
 }
 
